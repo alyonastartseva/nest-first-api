ec16775ddf1bfe2fe8fc70b8e3c2ebdf86fd12e0
Alyona <alyona_startseva.tk@mail.ru>
2021-11-15 19:51:43 +0700

    Added 4 modules: auth, product, review, top-page

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ReviewModule } from './review/review.module';
+import { ProductModule } from './product/product.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
