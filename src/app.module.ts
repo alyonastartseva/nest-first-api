782ea8c54cf6da3e76ae51b4785ea237d924f734
Alyona <alyona_startseva.tk@mail.ru>
2021-11-15 23:34:15 +0700

    Added @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { ProductModule } from './product/product.module';
 import { TopPageModule } from './top-page/top-page.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule, 
+    TopPageModule, 
+    ProductModule,
+    ReviewModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
